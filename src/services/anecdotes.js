bb3e5263e3686775508239ec3f78210373b537c0
m-lind <mari.j.lindstedt@gmail.com>
2023-10-27 16:31:08 +0300

    Added initial votes count for new anecdote and instruction for starting application

--- src/services/anecdotes.js
+++ src/services/anecdotes.js
@@ -9,6 +9,7 @@ const getAll = async () => {
 
 const createNew = async content => {
   const object = { content };
+  object.votes = 0;
   const response = await axios.post(baseUrl, object);
   return response.data;
 };
